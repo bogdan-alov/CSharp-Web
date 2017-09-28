16bff141ea5055fb04b8b27e68e19adbb13aab9e
Bogdan Alov <alovbg21@abv.bg>
2017-09-28 18:15:48 +0300

    added lab exercises for Web Server - HTTP Protocol lecture;

---
+++ CSharp Web Development Basics/03. Web Server - HTTP Protocol/Web Server - HTTP Protocol Lab/02. Validate URL/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _02._Validate_URL
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var url = Console.ReadLine();
+
+			bool isUri = Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute);
+
+			if (isUri)
+			{
+				var uri = new Uri(url);
+				Console.WriteLine($"Protocol: {uri.Scheme}");
+				Console.WriteLine($"Host: {uri.Host}");
+				Console.WriteLine($"Port: {uri.Port}");
+				Console.WriteLine($"Path: {uri.AbsolutePath}");
+				if (!string.IsNullOrEmpty(uri.Query))
+				{
+					Console.WriteLine($"Query: {uri.Query}");
+				}
+				if (!string.IsNullOrEmpty(uri.Fragment))
+				{
+					Console.WriteLine($"Fragment: {uri.Fragment}");
+				}
+			}
+			else
+			{
+				Console.WriteLine($"Invalid URL");
+			}
+			
+			
+				
+			
+
+		}
+	}
+}
---
+++ CSharp Web Development Basics/03. Web Server - HTTP Protocol/Web Server - HTTP Protocol Lab/03. Request Parser/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace _03._Request_Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var paths = new List<string>();
+	        var input = string.Empty;
+	        while ((input = Console.ReadLine()) != "END")
+	        {	
+		        paths.Add(input);
+	        }
+
+			var request = Console.ReadLine().Split();
+
+	        if (paths.Any(c=> c.EndsWith(request[0].ToLowerInvariant()) && c.Contains(request[1])))
+	        {
+			Console.WriteLine($"HTTP/1.1 {(int)HttpStatusCode.OK}");
+		        Console.WriteLine($"Content-Lenght:{HttpStatusCode.OK.ToString().Length}");
+		        Console.WriteLine($"Content-Type:text/plain");
+		        Console.WriteLine();
+		        Console.WriteLine(HttpStatusCode.OK.ToString());
+	        }
+	        else
+	        {
+
+		        Console.WriteLine($"HTTP/1.1 {(int)HttpStatusCode.NotFound}");
+		        Console.WriteLine($"Content-Lenght:{HttpStatusCode.NotFound.ToString().Length}");
+		        Console.WriteLine($"Content-Type:text/plain");
+		        Console.WriteLine();
+		        Console.WriteLine(HttpStatusCode.NotFound.ToString());
+			}
+        }
+    }
+}
